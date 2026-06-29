b92b8437c42e8a1d39e978c36c55ee52913e974d
Dhruv Bhatia <dhbhatia1410@gmail.com>
2026-06-29 18:37:02 +0530

    [1967] Number of Strings That Appear as Substrings in Word (cpp)

---
+++ Easy/1967-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+template<typename T> void lcPrint(const T& x) { cout << x; }
+inline void lcPrint(bool b) { cout << (b ? "true" : "false"); }
+inline void lcPrint(const string& s) { cout << '"' << s << '"'; }
+template<typename T> void lcPrint(const vector<T>& v) {
+    cout << '['; for (size_t i = 0; i < v.size(); ++i) { if (i) cout << ','; lcPrint(v[i]); } cout << ']';
+}
+template<typename A, typename B> void lcPrint(const pair<A,B>& p) {
+    cout << '('; lcPrint(p.first); cout << ','; lcPrint(p.second); cout << ')';
+}
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int num=0;
+        for(int i=0;i<patterns.size();i++){
+            if(word.find(patterns[i])!= string::npos){
+                num++;
+            }
+        }
+        return num;
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<string> patterns = {"a", "b", "c"};
+    string word = "abc";
+    auto __res = sol.numOfStrings(patterns, word);
+    lcPrint(__res);
+    cout << endl;
+    return 0;
+}
