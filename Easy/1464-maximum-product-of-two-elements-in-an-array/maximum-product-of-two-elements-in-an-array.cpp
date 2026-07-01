5534017f921447af2c9cb37a8e0e6ac640a52a68
Dhruv Bhatia <dhbhatia1410@gmail.com>
2026-07-02 03:04:13 +0530

    [1464] Maximum Product of Two Elements in an Array (cpp)

---
+++ Easy/1464-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+template<typename T> void lcPrint(const T& x) { cout << x; }
+inline void lcPrint(bool b) { cout << (b ? "true" : "false"); }
+inline void lcPrint(const string& s) { cout << '"' << s << '"'; }
+template<typename T> void lcPrint(const vector<T>& v) {
+    cout << '['; for (size_t i = 0; i < v.size(); ++i) { if (i) cout << ','; lcPrint(v[i]); } cout << ']';
+}
+template<typename A, typename B> void lcPrint(const pair<A,B>& p) {
+    cout << '('; lcPrint(p.first); cout << ','; lcPrint(p.second); cout << ')';
+}
+
+class compare{
+public:
+    bool operator()(int a , int b){
+        return a<b;
+    }
+};
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        priority_queue<int,vector<int>,compare>h;
+        for(int i=0;i<nums.size();i++){
+            h.push(nums[i]);
+        }
+        int prod=1;
+        for(int cnt=0;cnt<2;cnt++){
+            prod*=(h.top()-1);
+            h.pop();
+        }
+        return prod;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3};
+    auto __res = sol.maxProduct(nums);
+    lcPrint(__res);
+    cout << endl;
+    return 0;
+}
